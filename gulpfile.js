1811a4f02e5bbefd20de5e5c2ede11c616b5925a
Jan T. Sott <jan@idleberg.com>
2017-06-29 22:51:57 +0200

    add build task

--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,13 @@
  // Dependencies
 const gulp = require('gulp');
 const debug = require('gulp-debug');
-const eslint = require('gulp-eslint');
 const jsonlint = require('gulp-jsonlint');
+const svg2png = require('gulp-svg2png');
 const xmlVal = require('gulp-xml-validator');
 
 // Supported files
-const jsFiles = [
-  'lib/*.js',
-  'src/*.js',
+const tsFiles = [
+  'src/*.ts',
 ];
 
 const jsonFiles = [
@@ -28,18 +27,12 @@ const xmlFiles = [
   'syntaxes/*.tmLanguage'
 ];
 
-// Lint JavaScript
-gulp.task('lint:js', gulp.series(function(done) {
-  gulp.src(jsFiles)
-    .pipe(debug({title: 'eslint'}))
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-  done();
-}));
+const svgFiles = [
+  'src/logo.svg'
+];
 
 // Lint JSON
-gulp.task('lint:json', gulp.series(function(done) { 
+gulp.task('lint:json', gulp.series( (done) => { 
   gulp.src(jsonFiles)
     .pipe(debug({title: 'json-lint'}))
     .pipe(jsonlint())
@@ -49,14 +42,25 @@ gulp.task('lint:json', gulp.series(function(done) {
 }));
 
 // Validate XML
-gulp.task('lint:xml', gulp.series(function(done) { 
+gulp.task('lint:xml', gulp.series( (done) => { 
   gulp.src(xmlFiles)
     .pipe(debug({title: 'xml-validator'}))
     .pipe(xmlVal());
   done();
 }));
 
+// Convert SVG
+gulp.task('convert:svg', gulp.series( (done) => { 
+  gulp.src(svgFiles)
+    .pipe(svg2png({width: 128, height: 128}))
+    .pipe(gulp.dest('./images'));
+  done();
+}));
+
 // Available tasks
-gulp.task('lint', gulp.parallel('lint:js', 'lint:json', 'lint:xml', function(done) {
+gulp.task('lint', gulp.parallel('lint:json', 'lint:xml', (done) => {
+  done();
+}));
+gulp.task('build', gulp.parallel('convert:svg', (done) => {
   done();
 }));
