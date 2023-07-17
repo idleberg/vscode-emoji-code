a44a67232aa0319d32833c63aa33e6018d904d11
Jan T. Sott <git@idleberg.com>
2023-07-17 22:10:15 +0200

    update tooling

---
+++ gulpfile.mjs
@@ -0,0 +1,12 @@
+import gulp from 'gulp';
+import raster from 'gulp-raster';
+import rename from 'gulp-rename';
+
+// Convert SVG
+gulp.task('default', done => {
+  gulp.src('./src/logo.svg')
+    .pipe(raster())
+    .pipe(rename('logo.png'))
+    .pipe(gulp.dest('./images'));
+  done();
+});
