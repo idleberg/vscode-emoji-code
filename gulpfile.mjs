e89a3b5c25c8a403522ce4bdcfcff9a39d5ba982
Jan T. Sott <git@idleberg.com>
2023-10-15 11:58:47 +0200

    fix path

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -7,6 +7,6 @@ gulp.task('default', done => {
   gulp.src('./src/logo.svg')
     .pipe(raster())
     .pipe(rename('logo.png'))
-    .pipe(gulp.dest('./images'));
+    .pipe(gulp.dest('./resources'));
   done();
 });
