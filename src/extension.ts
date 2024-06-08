e868e18a532eba8523fd104e90ca31d9b1526052
Jan T. Sott <git@idleberg.com>
2024-06-08 22:59:14 +0200

    memoize functions

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,15 @@
 import { commands, type ExtensionContext, window} from 'vscode';
 import { getEmojiCode, listEmojis } from './emoji';
 import { insertText } from 'vscode-insert-text';
+import memoize from 'nano-memoize';
+
+const memoListEmojis = memoize(listEmojis);
+const memoEmojiCode = memoize(getEmojiCode);
 
 export function activate(context: ExtensionContext): void {
   context.subscriptions.push(
     commands.registerCommand('extension.emoji-code.open-palette', async () => {
-      const picks = listEmojis();
+      const picks = memoListEmojis();
 
       if (picks instanceof Error) {
         window.showErrorMessage(picks.message);
@@ -22,7 +26,7 @@ export function activate(context: ExtensionContext): void {
         return;
       }
 
-      const emojiCode = getEmojiCode(pick.label);
+      const emojiCode = memoEmojiCode(pick.label);
 
       if (emojiCode instanceof Error) {
         window.showErrorMessage(emojiCode.message);
