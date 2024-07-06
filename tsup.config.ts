5cd6b0c8f325dd89406da8ee4f2d10a588dd0647
Jan T. Sott <git@idleberg.com>
2024-07-06 22:38:05 +0200

    update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
+	bundle: true,
 	clean: true,
 	entry: ['src/extension.ts'],
 	external: ['vscode'],
 	format: 'cjs',
 	minify: true,
+  noExternal: ['emojibase-data', 'memoize', 'nano-memoize', 'vscode-insert-text'],
 	target: 'es2020',
 	outDir: 'lib',
+	treeshake: true,
+	cjsInterop: true,
+	platform: 'node',
 });
